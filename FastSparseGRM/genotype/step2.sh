0b0973728d3d229905c9ac824be9a3623b8c7068
YifanX1025 <yifan.xing@dph.ox.ac.uk>
2025-09-03 00:34:40 +0100

    Update step2.sh

--- FastSparseGRM/genotype/step2.sh
+++ FastSparseGRM/genotype/step2.sh
@@ -1,6 +1,12 @@
 # !/bin/bash
 
 dx run swiss-army-knife \
+  -iin="CRC WGS:/GRM/chrall_pruned.bed" \
+  -iin="CRC WGS:/GRM/chrall_pruned.bim" \
+  -iin="CRC WGS:/GRM/chrall_pruned.fam" \
+  -iin="CRC WGS:/GRM/output.seg" \
+  -iin="CRC WGS:/GRM/output.segments.gz" \
+  -iin="CRC WGS:/GRM/outputallsegs.txt" \
   -iin="CRC WGS:/GRM/FastSparseGRM/extdata/getDivergence_wrapper.R" \
   -iin="CRC WGS:/GRM/" \
   -y --brief \
@@ -8,12 +14,10 @@ dx run swiss-army-knife \
         # Install R packages
         Rscript -e "install.packages(\"devtools\"); if (!requireNamespace(\"BiocManager\", quietly = TRUE)) install.packages(\"BiocManager\", repos = \"https://cloud.r-project.org\"); BiocManager::install(version = \"3.20\", ask = FALSE); BiocManager::install(c(\"gdsfmt\", \"SNPRelate\", \"SeqArray\", \"SeqVarTools\")); install.packages(\"GMMAT\"); devtools::install_github(\"rounakdey/FastSparseGRM\")" &&
         
-
         # Run the divergence calculation
-        R CMD BATCH --vanilla "--args --prefix.in <prefix.bedfile> --file.seg <output.king> --num_threads 8 --degree 4 --divThresh -0.02209709 --nRandomSNPs 0 --prefix.out output.divergence" getDivergence_wrapper.R getDivergence.Rout
+        R CMD BATCH --vanilla "--args --prefix.in chrall_pruned --file.seg output --num_threads 32 --degree 4 --divThresh -0.02209709 --nRandomSNPs 50000 --prefix.out output.divergence" getDivergence_wrapper.R getDivergence.Rout
 
   " \
-  --instance-type mem2_ssd1_v2_x32 \
-  --name "GRM_step1" \
+  --instance-type mem1_ssd1_v2_x32 \
+  --name "GRM_step2" \
   --destination "CRC\ WGS:/GRM/"
-dx run swiss-army-knife -icmd='Rscript -e "install.packages(\"devtools\"); if (!requireNamespace(\"BiocManager\", quietly = TRUE)) install.packages(\"BiocManager\", repos = \"https://cloud.r-project.org\"); BiocManager::install(version = \"3.20\", ask = FALSE); BiocManager::install(c(\"gdsfmt\", \"SNPRelate\", \"SeqArray\", \"SeqVarTools\")); install.packages(\"GMMAT\"); devtools::install_github(\"rounakdey/FastSparseGRM\"); library(FastSparseGRM); packageVersion(\"FastSparseGRM\")"' --tag="FastSparseGRM_install" --instance-type="mem1_ssd1_v2_x4"
