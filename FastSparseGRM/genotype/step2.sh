ffaa241e8c0a739fcebf0412f90a5dd38049699b
YifanX1025 <yifan.xing@dph.ox.ac.uk>
2025-09-03 00:40:40 +0100

    Update step2.sh

--- FastSparseGRM/genotype/step2.sh
+++ FastSparseGRM/genotype/step2.sh
@@ -9,14 +9,13 @@ dx run swiss-army-knife \
   -iin="CRC WGS:/GRM/outputallsegs.txt" \
   -iin="CRC WGS:/GRM/FastSparseGRM/extdata/getDivergence_wrapper.R" \
   -y --brief \
-  -icmd="
+  -icmd='
         # Install R packages
         Rscript -e "install.packages(\"devtools\"); if (!requireNamespace(\"BiocManager\", quietly = TRUE)) install.packages(\"BiocManager\", repos = \"https://cloud.r-project.org\"); BiocManager::install(version = \"3.20\", ask = FALSE); BiocManager::install(c(\"gdsfmt\", \"SNPRelate\", \"SeqArray\", \"SeqVarTools\")); install.packages(\"GMMAT\"); devtools::install_github(\"rounakdey/FastSparseGRM\")" &&
         
         # Run the divergence calculation
         R CMD BATCH --vanilla "--args --prefix.in chrall_pruned --file.seg output --num_threads 32 --degree 4 --divThresh -0.02209709 --nRandomSNPs 50000 --prefix.out output.divergence" getDivergence_wrapper.R getDivergence.Rout
-
-  " \
+  ' \
   --instance-type mem1_ssd1_v2_x32 \
   --name "GRM_step2" \
   --destination "CRC WGS:/GRM/" \
