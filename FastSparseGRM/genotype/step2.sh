9b1204404dd281f5f245445fa68e4be8780dbdfc
YifanX1025 <yifan.xing@dph.ox.ac.uk>
2025-09-03 08:14:58 +0100

    Update step2.sh
    install packages

--- FastSparseGRM/genotype/step2.sh
+++ FastSparseGRM/genotype/step2.sh
@@ -10,8 +10,17 @@ dx run swiss-army-knife \
   -iin="CRC WGS:/GRM/FastSparseGRM/extdata/getDivergence_wrapper.R" \
   -y --brief \
   -icmd='
-    # Install R packages
-    Rscript -e "install.packages(\"devtools\"); if (!requireNamespace(\"BiocManager\", quietly = TRUE)) install.packages(\"BiocManager\", repos = \"https://cloud.r-project.org\"); BiocManager::install(version = \"3.20\", ask = FALSE); BiocManager::install(c(\"gdsfmt\", \"SNPRelate\", \"SeqArray\", \"SeqVarTools\")); install.packages(\"GMMAT\"); devtools::install_github(\"rounakdey/FastSparseGRM\")" &&
+    # Install R packages with dependencies
+    Rscript -e "
+      install.packages(c(\"devtools\", \"optparse\", \"data.table\", \"Matrix\"));
+      if (!requireNamespace(\"BiocManager\", quietly = TRUE)) install.packages(\"BiocManager\", repos = \"https://cloud.r-project.org\");
+      BiocManager::install(version = \"3.20\", ask = FALSE);
+      BiocManager::install(c(\"gdsfmt\", \"SNPRelate\", \"SeqArray\", \"SeqVarTools\"));
+      install.packages(\"GMMAT\");
+      devtools::install_github(\"rounakdey/FastSparseGRM\");
+      library(FastSparseGRM);
+      packageVersion(\"FastSparseGRM\")
+    " &&
     pwd && ls -la && cd /home/dnanexus/out/out && 
     # Run the divergence calculation
     R CMD BATCH --vanilla "--args --prefix.in chrall_pruned --file.seg output --num_threads 24 --degree 4 --divThresh -0.02209709 --nRandomSNPs 50000 --prefix.out output.divergence" getDivergence_wrapper.R getDivergence.Rout && 
