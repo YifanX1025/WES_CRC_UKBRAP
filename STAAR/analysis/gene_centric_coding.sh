8f738b7032fed1a38597231606fd60f16f08546a
YifanX1025 <yifan.xing@dph.ox.ac.uk>
2025-09-05 23:57:23 +0100

    Update gene_centric_coding.sh

--- STAAR/analysis/gene_centric_coding.sh
+++ STAAR/analysis/gene_centric_coding.sh
@@ -1,16 +1,16 @@
 # !/bin/bash
 ### CRC WES unconditional analysis for gene-centric coding
-nullmodel="CRC WGS:/STAAR/Nullmodel/.Rdata"
-path="CRC WGS:/STAAR/Gene_Centric_Coding/"
+nullmodel="project-GyJ14jjJxy674xQ2pGQ5G3K6:/STAAR/Nullmodel/.Rdata"
+path="project-GyJ14jjJxy674xQ2pGQ5G3K6:/STAAR/Gene_Centric_Coding/"
 
 for CHR in {1..22}; do
-dx run CRC\ WGS:/staarpipeline --priority=low \
+dx run project-GyJ14jjJxy674xQ2pGQ5G3K6:/staarpipeline --priority=normal \
 -inullobj_file=${nullmodel} \
--iagds_file=CRC\ WGS:/UKB_500k_WGS_aGDS/ukb.500k.wgs.chr${CHR}.pass.annotated.gds \
--iannotation_name_catalog_file=cdbg_lipids_staartopmed:/TOPMed_lipids_F8_staarpipeline/Annotation_name_catalog.csv \
--itest_type=Gene_Centric_Coding \
+-iagds_file=project-GyJ14jjJxy674xQ2pGQ5G3K6:/UKB_500k_WGS_aGDS/ukb.500k.wgs.chr${CHR}.pass.annotated.gds \
+-iannotation_name_catalog_file=project-GyJ14jjJxy674xQ2pGQ5G3K6:/STAAR/Annotation_name_catalog.csv \
+-itest_type="Gene_Centric_Coding" \
 -iuser_cores=40 \
--ioutfile=LDL_F8_gene_centric_coding_chr${CHR} \
+-ioutfile=crc_wes_gene_centric_coding_chr${CHR} \
 -iannotation_dir=annotation/info/FunctionalAnnotation \
---destination=${path} --instance-type="mem3_ssd1_v2_x48" --yes
+--destination=${path} --instance-type=mem3_ssd1_v2_x48 --yes
 done
