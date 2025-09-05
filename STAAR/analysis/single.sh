45b3c35dbf7f5dd22b534fc6463fe4db9190a530
YifanX1025 <yifan.xing@dph.ox.ac.uk>
2025-09-05 23:45:49 +0100

    Update single.sh

--- STAAR/analysis/single.sh
+++ STAAR/analysis/single.sh
@@ -1,14 +1,14 @@
 # !/bin/bash
 
 ### CRC WES unconditional analysis for single
-nullmodel="CRC WGS:/STAAR/Nullmodel/.Rdata"
-path="CRC WGS:/STAAR/Single/"
+nullmodel="project-GyJ14jjJxy674xQ2pGQ5G3K6:/STAAR/Nullmodel/.Rdata"
+path="project-GyJ14jjJxy674xQ2pGQ5G3K6:/STAAR/Single/"
 
 for CHR in {1..22}; do
-dx run CRC\ WGS:/staarpipeline --priority=normal \
+dx run project-GyJ14jjJxy674xQ2pGQ5G3K6:/staarpipeline --priority=normal \
 -inullobj_file=${nullmodel} \
--iagds_file=CRC\ WGS:/UKB_500k_WGS_aGDS/ukb.500k.wgs.chr${CHR}.pass.annotated.gds \
--itest_type=Single \
+-iagds_file=project-GyJ14jjJxy674xQ2pGQ5G3K6:/UKB_500k_WGS_aGDS/ukb.500k.wgs.chr${CHR}.pass.annotated.gds \
+-itest_type="Single" \
 -iuser_cores=16 \
 -ioutfile=crc_wes_single_chr${CHR} \
 --destination=${path} --instance-type="mem3_ssd1_v2_x64" --yes
