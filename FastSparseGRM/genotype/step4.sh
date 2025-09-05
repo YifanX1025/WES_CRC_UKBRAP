4328fcd74d9e7aca84ebdbe60e2d394e3f5ec398
YifanX1025 <yifan.xing@dph.ox.ac.uk>
2025-09-05 20:54:50 +0100

    Update step4.sh

--- FastSparseGRM/genotype/step4.sh
+++ FastSparseGRM/genotype/step4.sh
@@ -1,30 +1,31 @@
 # !/bin/bash
+# Use PCs from UKB
 
-dx run swiss-army-knife \
-  -iin="CRC WGS:/GRM/chrall_pruned.bed" \
-  -iin="CRC WGS:/GRM/chrall_pruned.bim" \
-  -iin="CRC WGS:/GRM/chrall_pruned.fam" \
-  -iin="CRC WGS:/GRM/output.unrelated.unrels" \
-  -iin="CRC WGS:/GRM/FastSparseGRM/extdata/runPCA_wrapper.R" \
-  -y --brief \
-  -icmd='
-    # Install R packages with dependencies
-    Rscript -e "
-      install.packages(c(\"devtools\", \"optparse\", \"data.table\", \"Matrix\"));
-      if (!requireNamespace(\"BiocManager\", quietly = TRUE)) install.packages(\"BiocManager\", repos = \"https://cloud.r-project.org\");
-      BiocManager::install(version = \"3.20\", ask = FALSE);
-      BiocManager::install(c(\"gdsfmt\", \"SNPRelate\", \"SeqArray\", \"SeqVarTools\"));
-      install.packages(\"GMMAT\");
-      devtools::install_github(\"rounakdey/FastSparseGRM\");
-      library(FastSparseGRM);
-      packageVersion(\"FastSparseGRM\")
-    " &&
-    pwd && ls -la && cd /home/dnanexus/out/out && 
-    # Run the divergence calculation
-    R  CMD BATCH --vanilla "--args --prefix.in chrall_pruned --file.unrels output.unrelated.unrels --prefix.out output.pca --no_pcs 20 --num_threads 24 --no_iter 10" runPCA_wrapper.R runPCA.Rout && 
-    tail -n 200 runPCA.Rout || true
-  ' \
-  --instance-type mem2_ssd1_v2_x32 \
-  --name "GRM_step4" \
-  --destination "CRC WGS:/GRM/" \
-  --priority high
+#dx run swiss-army-knife \
+#  -iin="CRC WGS:/GRM/chrall_pruned.bed" \
+#  -iin="CRC WGS:/GRM/chrall_pruned.bim" \
+#  -iin="CRC WGS:/GRM/chrall_pruned.fam" \
+#  -iin="CRC WGS:/GRM/output.unrelated.unrels" \
+#  -iin="CRC WGS:/GRM/FastSparseGRM/extdata/runPCA_wrapper.R" \
+#  -y --brief \
+#  -icmd='
+#    # Install R packages with dependencies
+#    Rscript -e "
+#      install.packages(c(\"devtools\", \"optparse\", \"data.table\", \"Matrix\"));
+#      if (!requireNamespace(\"BiocManager\", quietly = TRUE)) install.packages(\"BiocManager\", repos = \"https://cloud.r-project.org\");
+#      BiocManager::install(version = \"3.20\", ask = FALSE);
+#      BiocManager::install(c(\"gdsfmt\", \"SNPRelate\", \"SeqArray\", \"SeqVarTools\"));
+#      install.packages(\"GMMAT\");
+#      devtools::install_github(\"rounakdey/FastSparseGRM\");
+#      library(FastSparseGRM);
+#      packageVersion(\"FastSparseGRM\")
+#    " &&
+#    pwd && ls -la && cd /home/dnanexus/out/out && 
+#    # Run the divergence calculation
+#    R  CMD BATCH --vanilla "--args --prefix.in chrall_pruned --file.unrels output.unrelated.unrels --prefix.out output.pca --no_pcs 20 --num_threads 24 --no_iter 10" runPCA_wrapper.R runPCA.Rout && 
+#    tail -n 200 runPCA.Rout || true
+#  ' \
+#  --instance-type mem2_ssd1_v2_x32 \
+#  --name "GRM_step4" \
+#  --destination "CRC WGS:/GRM/" \
+#  --priority high
