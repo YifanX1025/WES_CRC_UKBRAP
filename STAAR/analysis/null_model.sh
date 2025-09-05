60e72e777d4e4509e564dd5420d9b7f486bef940
YifanX1025 <yifan.xing@dph.ox.ac.uk>
2025-09-05 23:39:40 +0100

    Update null_model.sh

--- STAAR/analysis/null_model.sh
+++ STAAR/analysis/null_model.sh
@@ -2,17 +2,22 @@
 # https://docs.google.com/document/d/1-3PxZ7r5ZqOBQmr7i-LOPLbd2PpYuMBpv3p2k3AQ4Q0/edit?pli=1&tab=t.0
 ###### RUN NULL MODEL AND WAIT FOR IT TO COMPLETE ######
 
-### CRC WES null model
+# Define variables with proper quoting
 pheno="CRC WGS:/STAAR/phenotype_covars_combined.csv"
 path="CRC WGS:/STAAR/Nullmodel/"
+grm_file="CRC WGS:/GRM/converted.sparseGRM.sGRM.RData"
+app_name="CRC WGS:/staarpipeline"
 
-dx run CRC\ WGS:/staarpipeline \
--ipheno_file=${pheno} \
--igrm_file=CRC\ WGS:/GRM/converted.sparseGRM.sGRM.RData \
+# Run the dx command with proper quoting
+dx run "${app_name}" \
+-ipheno_file="${pheno}" \
+-igrm_file="${grm_file}" \
 -ipheno_id=eid \
 -icovariates="age_c,age2_c,sex,age_c_sex,age2_c_sex,PC1,PC2,PC3,PC4,PC5,PC6,PC7,PC8,PC9,PC10" \
 -iphenotype=case_status \
 -ioutfile="crc_wes_nullmodel" \
 -itest_type="Null" \
 --priority=normal \
---destination=${path} --instance-type="mem3_ssd1_v2_x4" --yes
+--destination="${path}" \
+--instance-type="mem3_ssd1_v2_x4" \
+--yes
