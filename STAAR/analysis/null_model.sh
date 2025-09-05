f678c3c6da4acad97ac8d115492bc5562d26b683
YifanX1025 <yifan.xing@dph.ox.ac.uk>
2025-09-05 23:20:37 +0100

    Update null_model.sh

--- STAAR/analysis/null_model.sh
+++ STAAR/analysis/null_model.sh
@@ -2,18 +2,17 @@
 # https://docs.google.com/document/d/1-3PxZ7r5ZqOBQmr7i-LOPLbd2PpYuMBpv3p2k3AQ4Q0/edit?pli=1&tab=t.0
 ###### RUN NULL MODEL AND WAIT FOR IT TO COMPLETE ######
 
-### Example: TOPMed lipids F8 null model
+### CRC WES null model
 pheno="CRC WGS:/STAAR/phenotype_covars_combined.csv"
-path="CRC WGS:/STAAR"
+path="CRC WGS:/STAAR/"
 
-dx run Commons_Tools:/staarpipeline_v0.9.6 \
+dx run CRC\ WGS:/staarpipeline \
 -ipheno_file=${pheno} \
--igrm_file=cdbg:/GRMs/freeze.8/pcrelate_kinshipMatrix_sparseDeg4_v2.Rda \
--ipheno_id=sample.id \
--icovariates="age,age2,sex,group,PC1,PC2,PC3,PC4,PC5,PC6,PC7,PC8,PC9,PC10,PC11" \
--ihet_vars="group" \
--iphenotype=LDL_ADJ.norm \
--ioutfile="LDL_F8_nullmodel" \
+-igrm_file=CRC\ WGS:/GRM/converted.sparseGRM.sGRM.RData \
+-ipheno_id=eid \
+-icovariates="age_c,age2_c,sex,age_c_sex,age2_c_sex,PC1,PC2,PC3,PC4,PC5,PC6,PC7,PC8,PC9,PC10" \
+-iphenotype=case_status \
+-ioutfile="crc_wes_nullmodel" \
 -itest_type="Null" \
---priority=low \
+--priority=normal \
 --destination=${path} --instance-type="mem3_ssd1_v2_x4" --yes
