76ddb48405e221e745e275ec038b50613871414e
YifanX1025 <yifan.xing@dph.ox.ac.uk>
2025-09-06 13:25:54 +0100

    Update annofilter_NA2PASS.sh

--- STAAR/annofilter_NA2PASS.sh
+++ STAAR/annofilter_NA2PASS.sh
@@ -7,7 +7,7 @@ for chr in {1..22}; do
     -icmd="
       echo 'Processing chromosome $chr...' && 
       gzcat ukb23157_c${chr}_merged_v1_staar_trimmed.vcf.gz | awk 'BEGIN{OFS="\t"} /^#/ {print} !/^#/ {if($7==".") $7="PASS"; print}' | bgzip > ukb23157_c${chr}_merged_v1_staar_trimmed_fixed.vcf.gz && 
-      tabix -p vcf ukb23157_c${chr}_merged_v1_staar_trimmed_fixed.vcf.gz
+      bcftools index -t ukb23157_c${chr}_merged_v1_staar_trimmed_fixed.vcf.gz
     " \
     --instance-type mem1_ssd1_v2_x2 \
     --name "fix_vcf_chr${chr}" \
