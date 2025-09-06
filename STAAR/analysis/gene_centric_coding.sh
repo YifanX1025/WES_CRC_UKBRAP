595b7061e247e4fde2d375df5c84e4e58150ea56
YifanX1025 <yifan.xing@dph.ox.ac.uk>
2025-09-06 01:25:13 +0100

    Update gene_centric_coding.sh

--- STAAR/analysis/gene_centric_coding.sh
+++ STAAR/analysis/gene_centric_coding.sh
@@ -1,10 +1,10 @@
 # !/bin/bash
 ### CRC WES unconditional analysis for gene-centric coding
-nullmodel="project-GyJ14jjJxy674xQ2pGQ5G3K6:/STAAR/Nullmodel/.Rdata"
+nullmodel="project-GyJ14jjJxy674xQ2pGQ5G3K6:/STAAR/Nullmodel/crc_wes_nullmodel.Rdata"
 path="project-GyJ14jjJxy674xQ2pGQ5G3K6:/STAAR/Gene_Centric_Coding/"
 
 for CHR in {1..22}; do
-dx run project-GyJ14jjJxy674xQ2pGQ5G3K6:/staarpipeline --priority=normal \
+dx run project-GyJ14jjJxy674xQ2pGQ5G3K6:/staarpipeline --priority=low \
 -inullobj_file=${nullmodel} \
 -iagds_file=project-GyJ14jjJxy674xQ2pGQ5G3K6:/UKB_500k_WGS_aGDS/ukb.500k.wgs.chr${CHR}.pass.annotated.gds \
 -iannotation_name_catalog_file=project-GyJ14jjJxy674xQ2pGQ5G3K6:/STAAR/Annotation_name_catalog.csv \
