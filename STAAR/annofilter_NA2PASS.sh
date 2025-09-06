17a8ad80227d1ba231a3dcb37a2b6317d5d01f39
YifanX1025 <yifan.xing@dph.ox.ac.uk>
2025-09-06 13:29:05 +0100

    Update annofilter_NA2PASS.sh

--- STAAR/annofilter_NA2PASS.sh
+++ STAAR/annofilter_NA2PASS.sh
@@ -2,11 +2,11 @@
 
 for chr in {1..22}; do
   dx run swiss-army-knife \
-    -iin="ukb23157_c${chr}_merged_v1_staar_trimmed.vcf.gz" \
-    -iin="ukb23157_c${chr}_merged_v1_staar_trimmed.vcf.gz.tbi" \
+    -iin="CRC WGS:/Step2_vcf_merged_500k/ukb23157_c${chr}_merged_v1_staar_trimmed.vcf.gz" \
+    -iin="CRC WGS:/Step2_vcf_merged_500k/ukb23157_c${chr}_merged_v1_staar_trimmed.vcf.gz.tbi" \
     -icmd="
-      echo 'Processing chromosome $chr...' && 
-      gzcat ukb23157_c${chr}_merged_v1_staar_trimmed.vcf.gz | awk 'BEGIN{OFS="\t"} /^#/ {print} !/^#/ {if($7==".") $7="PASS"; print}' | bgzip > ukb23157_c${chr}_merged_v1_staar_trimmed_fixed.vcf.gz && 
+      echo 'Processing chromosome ${chr}...' && 
+      gzcat ukb23157_c${chr}_merged_v1_staar_trimmed.vcf.gz | awk 'BEGIN{OFS=\"\t\"} /^#/ {print} !/^#/ {if(\$7==\".\") \$7=\"PASS\"; print}' | bgzip > ukb23157_c${chr}_merged_v1_staar_trimmed_fixed.vcf.gz && 
       bcftools index -t ukb23157_c${chr}_merged_v1_staar_trimmed_fixed.vcf.gz
     " \
     --instance-type mem1_ssd1_v2_x2 \
