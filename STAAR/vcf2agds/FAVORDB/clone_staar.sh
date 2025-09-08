139e05d2877ef5aacdf16e44fe932d69a7049cac
YifanX1025 <yifan.xing@dph.ox.ac.uk>
2025-09-08 13:04:53 +0100

    Update clone_staar.sh

--- STAAR/vcf2agds/FAVORDB/clone_staar.sh
+++ STAAR/vcf2agds/FAVORDB/clone_staar.sh
@@ -1,2 +1,13 @@
 # !/bin/bash
 
+dx run swiss-army-knife \
+  -icmd="
+  apt-get update && apt-get -y install git && 
+  git clone https://github.com/xihaoli/STAAR.git
+  " \
+  --instance-type "mem1_ssd1_v2_x2" \
+  --name "staar_git" \
+  --destination "CRC WGS:/STAAR/" \
+  --yes \
+  --brief \
+  --priority high
