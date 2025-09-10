0eb51c6e2021445c6ddec9d5b915c6bdeaae5c73
YifanX1025 <yifan.xing@dph.ox.ac.uk>
2025-09-10 21:43:57 +0100

    Update gene_centric_coding.sh

--- STAAR/analysis/gene_centric_coding.sh
+++ STAAR/analysis/gene_centric_coding.sh
@@ -6,7 +6,7 @@ path="project-GyJ14jjJxy674xQ2pGQ5G3K6:/STAAR/Gene_Centric_Coding/"
 for CHR in {1..22}; do
 dx run project-GyJ14jjJxy674xQ2pGQ5G3K6:/staarpipeline --priority=low \
 -inullobj_file=${nullmodel} \
--iagds_file=project-GyJ14jjJxy674xQ2pGQ5G3K6:/UKB_500k_WGS_aGDS/ukb.500k.wgs.chr${CHR}.pass.annotated.gds \
+-iagds_file=project-GyJ14jjJxy674xQ2pGQ5G3K6:/UKB_500k_WGS_aGDS/filtered/ukb.500k.wes.chr${CHR}.pass.annotated.gds \
 -iannotation_name_catalog_file=project-GyJ14jjJxy674xQ2pGQ5G3K6:/STAAR/Annotation_name_catalog.csv \
 -itest_type="Gene_Centric_Coding" \
 -iuser_cores=40 \
